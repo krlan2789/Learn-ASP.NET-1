954ac37ac319066fc74b2db1cac96d90c522ffc4
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-12-24 14:19:44 +0700

    Add OpenAPI and Scalar for documentation . .

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,12 +1,32 @@
 using GameStore.API.Data;
 using GameStore.API.Endpoints;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Database Context services
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
+// Adds OpenAPI services
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    string openApiRoute = "/docs/{documentName}/openapi.json";
+    // Register the endpoint for viewing the OpenAPI
+    app.MapOpenApi(openApiRoute);
+    app.MapScalarApiReference(options =>
+    {
+        string scalarApiRoute = "/docs/{documentName}";
+        options
+            .WithTitle("Game Store - REST API")
+            .WithEndpointPrefix(scalarApiRoute)
+            .WithOpenApiRoutePattern(openApiRoute);
+    });
+}
 
 // Register all endpoints
 app.MapGamesEndpoints();
